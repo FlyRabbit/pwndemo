df36423a0a11720f943677a81e5ddc2b227352d5
explorer <hsadkhk@gmail.com>
2016-03-07 21:52:06 +0800

    add permission

--- pwnlib/daemons/daemon.py
+++ pwnlib/daemons/daemon.py
@@ -1,6 +1,9 @@
+import os
 import time
 
 import subprocess
+from pwd import getpwnam
+from random import randint
 
 from .. import context
 from ..timeout import Timeout
@@ -35,6 +38,9 @@ class daemon(Timeout):
                  stderr = STDOUT,
                  close_fds = True,
                  preexec_fn = lambda: None):
+        cwd = cwd or os.path.curdir
+        if cwd != '/':
+            cwd += '/'
         self.argv = argv
         self.shell =shell
         self.executable =executable
@@ -47,11 +53,12 @@ class daemon(Timeout):
         self.close_fds = close_fds
         self.preexec_fn = preexec_fn
 
-    def __call__(self):
+    def __call__(self,getFlag = None):
         with listened(self.port, self.bindaddr, self.fam, self.typ, self.Timeout) as listen:
             try:
                 if listen == None:
                     return
+                self._set_env(getFlag)
                 process = tubes.process.process(self.argv,
                                                 self.shell,
                                                 self.executable,
@@ -73,7 +80,29 @@ class daemon(Timeout):
                     if not self.countdown_active():
                         listen.sendline('Sorry timeout')
                 process.close()
+                self._clear_env()
             except KeyboardInterrupt:
                 exit(0)
 
+    def _set_env(self, getFlag):
+        self.username = 'pwnuser%d'%(os.getpid(),)
+        self.cwd = self.cwd + self.username
+        os.makedirs(self.cwd,0755)
+        os.system('useradd -p "" -s "/usr/sbin/nologin" -d "{}" {}'.format(self.cwd, self.username))
+        os.system('chown -hR {0}:{0} {1}/ '.format(self.username,self.cwd[:-1]))
+        os.system('chmod -R 0750 ' + self.cwd)
+        os.system('echo "%s" >> %s/flag%d'%(getFlag(), self.cwd, randint(10000,99999)))
+
+        pw = getpwnam(self.username)
+        uid = pw.pw_uid
+        gid = pw.pw_gid
+
+        os.setgroups([gid])
+        os.setgid(gid)
+        os.setuid(uid)
+
+    def _clear_env(self):
+        os.system('killall -u {} -9'.format(self.username))
+        os.system('userdel -r ' + self.username)
+        os.system('groupdel ' + self.username)
 
